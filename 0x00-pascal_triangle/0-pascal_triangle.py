620f829cef2ca780e07101bfa2315b6045ed95e5
jenanouar1234 <jenanouar1234@gmail.com>
2024-10-06 13:26:30 +0100

    pushed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+This module defines the function pascal_triangle generates Pascal triangle.
+"""
+
+
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing Pascal's triangle of n.
+    Generates Pascal's triangle up to n rows.
+
+    Args:
+        n (int): The number of rows in Pascal's triangle.
+
+    Returns:
+        list of lists: A list of lists where each inner list represents a row
+        of Pascal's triangle.
     """
-    if n <= 0:
-        return []
+    res = []  # Initialize the result list to store levels of Pascal Triangle
 
-    # Initialize the triangle with the first row
-    triangle = [[1]]
+    if n > 0:
+        # Loop through the number of rows
+        for i in range(1, n + 1):
+            level = []  # Initialize a new row (level)
+            C = 1  # The first value of every row is always 1
 
-    for i in range(1, n):
-        # Start each row with a '1'
-        row = [1]
+            # Populate each row with the correct values
+            for j in range(1, i + 1):
+                level.append(C)  # Append the current value of C
+                C = C * (i - j) // j
 
-        # Add the values from the previous row to form the new row
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            res.append(level)  # Add the completed row to the result list
 
-        # End each row with a '1'
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    return res  # Return the full Pascal's Triangle as a list of lists
