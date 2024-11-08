c4e7bb61b56187747fb990a1f5b8d7abb9986ed2
jenanouar1234 <jenanouar1234@gmail.com>
2024-11-08 12:52:36 +0100

    pushed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,64 +1,44 @@
 #!/usr/bin/python3
-"""N Queens Puzzle Solver"""
+""" N queens """
 import sys
 
 
-if len(sys.argv) != 2:
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 
-# Check if N is a valid integer
-try:
-    n = int(sys.argv[1])
-except ValueError:
+if not sys.argv[1].isdigit():
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
-# Check if N is at least 4
-if n < 4:
+if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
+
+n = int(sys.argv[1])
 
 
-def solve_nqueens(n):
-    """Solve the N queens puzzle and print all solutions"""
-    def backtrack(row, diagonals, anti_diagonals, cols, state):
-        # If all queens are placed
-        if row == n:
-            solution = [[r, c] for r, c in enumerate(state)]
-            solutions.append(solution)
-            return
-        
-        for col in range(n):
-            curr_diag = row - col
-            curr_anti_diag = row + col
-
-            # Check if the queen is under attack in the column or diagonals
-            if (col in cols or
-                curr_diag in diagonals or
-                curr_anti_diag in anti_diagonals):
-                continue
-
-            # Place the queen
-            cols.add(col)
-            diagonals.add(curr_diag)
-            anti_diagonals.add(curr_anti_diag)
-            state.append(col)
-
-            # Move to the next row
-            backtrack(row + 1, diagonals, anti_diagonals, cols, state)
-
-            # Remove the queen
-            cols.remove(col)
-            diagonals.remove(curr_diag)
-            anti_diagonals.remove(curr_anti_diag)
-            state.pop()
-
-    solutions = []
-    backtrack(0, set(), set(), set(), [])
-    return solutions
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-# Print each solution
-for solution in solve_nqueens(n):
-    print(solution)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
